083b2a5df805f115f1d49e58accadc06d835da31
Asel Atakeev <aselya.atakeeva@gmail.com>
2022-08-12 14:23:36 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+ import Login from './Login';
+ import Values from './Values';
+ import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ class App extends Component {
+   constructor(props) {
+     super(props);
+     this.state = { token: "", username: "", password: "", values: [], error: "" };
+   }
 
-export default App;
+   onLogin = () => {
+     fetch("http://localhost:8080/login", {
+     headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+     method: "POST",
+     body: JSON.stringify({ username: this.state.username, password: this.state.password })
+     })
+     .then(res => res.headers.get("authorization"))
+     .then(token => {
+       if (token) {
+         this.setState({ ...this.state, token: token });
+       } else {
+         this.setState({ ...this.state, error: "Unable to login with username and password." });
+       }
+     });
+   }
+
+   onUsernameChange = (e) => this.setState({ ...this.state, username: e.target.value });
+   onPasswordChange = (e) => this.setState({ ...this.state, password: e.target.value });
+
+   onGetValues = () => {
+     fetch("http://localhost:8080/api/values", {
+       headers: { 'Authorization': this.state.token }
+     })
+     .then(res => res.json())
+     .then(json => this.setState({ ...this.state, values: json }));
+   }
+
+   render() {
+     return (
+     <div className="App">
+       <header className="App-header">
+       {(!this.state.token || this.state.token === "")
+         ? (<Login onUsernameChange={this.onUsernameChange}
+         onPasswordChange={this.onPasswordChange}
+         onLogin={this.onLogin}
+         error={this.state.error}></Login>)
+         : (<Values values={this.state.values} onGetValues={this.onGetValues}></Values>)}
+       </header>
+     </div>
+     );
+   }
+ }
+
+ export default App;
